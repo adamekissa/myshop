53368267a0fede086dcf1761020ec0638fcb9a64
adamekissa <adamekissa@gmail.com>
2022-03-30 19:25:15 +0100

    initialised

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+import "./Home.css";
+
+
+function Home(){
+    return( 
+        <div className="grid-container">
+            <div className="grid1"><h1>GRID 1</h1> </div>
+            <div className="grid2"><h1>GRID 2</h1> </div>
+            <div className="grid3"><h1>GRID 3</h1> </div>
+            <div className="grid4"><h1>GRID 4</h1> </div>
+        </div>
+)};
+export default Home;
